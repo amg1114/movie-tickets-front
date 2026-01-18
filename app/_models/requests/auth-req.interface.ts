c62afd98b9ddb8375c1ee61b4bd098955d4db892
Alejandro Moreno <alejandromogil@gmail.com>
2026-01-18 10:51:45 -0300

    feat: add user and authentication request/response interfaces

---
+++ app/_models/requests/auth-req.interface.ts
@@ -0,0 +1,11 @@
+export interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface IRegisterRequest {
+  name: string;
+  phone: string;
+  email: string;
+  password: string;
+}
---
+++ app/_models/responses/auth-res.interface.ts
@@ -0,0 +1,7 @@
+import { IUser } from "../entities/user.interface";
+
+export interface IAuthResponse {
+    access_token: string;
+    user:         IUser;
+}
+
