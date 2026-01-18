1b39a63330d2c6d40da4c9f8abc3f56a33adb006
Alejandro Moreno <82889900+amg1114@users.noreply.github.com>
2026-01-18 11:55:58 -0300

    Add authentication forms and improve project structure (#1)
    * setup: add axios, clsx, react-hook-form, and zod dependencies
    
    * style: update globals.css for font variables and add fonts.ts for Google Fonts integration
    
    * refactor: update layout to use custom fonts and add background image
    
    * feat: add StyledInput component with label support
    
    * setup: add lucide-react and @hookform/resolvers dependencies
    
    * setup: create fonts.ts for Google Fonts integration and update import path in layout.tsx
    
    * feat: implement StyledInput component with error handling and label support
    
    * feat: add user and authentication request/response interfaces
    
    * feat: add FormFeedback component for displaying form submission status
    
    * feat: setup EditorConfig, Prettier, ESLint, and VSCode settings for consistent code formatting
    
    * feat: implement login and registration forms with validation and API integration
    
    * feat: implement authentication context and protected routes with login and registration functionality
    
    * refactor: enhance project structure with grouped feature pages by concept and separate layouts
    
    * feat: update import paths for consistency and enhance TypeScript configuration

---
+++ app/(features)/(auth)/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import StyledInput from "@/_ui/components/forms/StyledInput";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/_utils/schemas/user.schema";
+import { ILoginRequest } from "@/_models/requests/auth-req.interface";
+import { useEffect, useState } from "react";
+import FormFeedback, { IFormFeedbackProps } from "@/_ui/components/forms/FormFeedback";
+import { AxiosError } from "axios";
+import { useAuth } from "@/_context/AuthContext";
+
+export default function Login() {
+  const router = useRouter();
+  const { login: loginUser, isAuthenticated, isLoading } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginRequest>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  useEffect(() => {
+    if (isAuthenticated && !isLoading) {
+      router.push("/shows");
+    }
+  }, [isAuthenticated, isLoading, router]);
+
+  const [state, setState] = useState<{
+    error?: string;
+    status: IFormFeedbackProps["status"];
+  } | null>(null);
+
+  const onSubmit = async (data: ILoginRequest) => {
+    setState({ status: "loading" });
+    try {
+      await loginUser(data);
+      setState({ status: "success" });
+
+      setTimeout(() => router.push("/shows"), 1000);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setState({
+          status: "error",
+          error: error.response?.data.message || "An error occurred",
+        });
+
+        return;
+      }
+
+      setState({ status: "error", error: "An error occurred" });
+    }
+  };
+
+  return (
+    <form
+      className="w-full max-w-sm rounded p-4 sm:min-w-sm sm:p-8"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-4">
+        <StyledInput
+          label="E-mail"
+          id="email"
+          type="text"
+          {...register("email")}
+          errors={errors.email?.message}
+        />
+
+        <StyledInput
+          label="Password"
+          id="password"
+          type="password"
+          {...register("password")}
+          errors={errors.password?.message}
+        />
+
+        {state && (
+          <FormFeedback
+            status={state.status}
+            errorMessage={state.error}
+            successMessage="Login successful! Redirecting..."
+          />
+        )}
+
+        <footer className="flex flex-col gap-4 text-sm">
+          <Link href="/register" className="text-end underline hover:text-purple-700">
+            You don&apos;t have an account?
+          </Link>
+          <button
+            type="submit"
+            className="w-full rounded bg-purple-600 px-4 py-2 font-bold text-white hover:bg-purple-700"
+          >
+            Log In
+          </button>
+        </footer>
+      </div>
+    </form>
+  );
+}
---
+++ app/_utils/schemas/user.schema.tsx
@@ -0,0 +1,31 @@
+import z from "zod";
+
+const passwordRequirements = z
+  .string({
+    error: "Password is required",
+  })
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .regex(/[A-Z]/, {
+    message: "Password must contain at least one uppercase letter",
+  })
+  .regex(/[a-z]/, {
+    message: "Password must contain at least one lowercase letter",
+  })
+  .regex(/[0-9]/, { message: "Password must contain at least one number" })
+  .regex(/[\W_]/, {
+    message: "Password must contain at least one special character",
+  });
+
+export const loginSchema = z.object({
+  email: z.email({ message: "Invalid email address" }),
+  password: passwordRequirements,
+});
+
+export const registerSchema = z.object({
+  name: z.string().min(2, { message: "Name must be at least 2 characters long" }),
+  phone: z.string().regex(/^(\+?57)?3\d{9}$/, {
+    message: "Invalid phone number (e.g., 3001234567).",
+  }),
+  email: z.email({ message: "Invalid email address" }),
+  password: passwordRequirements,
+});
